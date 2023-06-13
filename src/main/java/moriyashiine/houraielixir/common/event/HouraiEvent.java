a8313f8a822c07ad8e5ed2202fd4a34593979a88
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2023-06-13 14:40:33 -0400

    update to 1.20.1

--- src/main/java/moriyashiine/houraielixir/common/event/HouraiEvent.java
+++ src/main/java/moriyashiine/houraielixir/common/event/HouraiEvent.java
@@ -22,18 +22,18 @@ public class HouraiEvent implements ServerLivingEntityEvents.AllowDeath {
 	@Override
 	public boolean allowDeath(LivingEntity entity, DamageSource damageSource, float damageAmount) {
 		if (HouraiElixir.isImmortal(entity)) {
-			entity.world.playSound(null, entity.getBlockPos(), ModSoundEvents.ENTITY_GENERIC_RESURRECT, entity.getSoundCategory(), 1, 1);
-			if (entity.getY() <= entity.world.getBottomY() && damageSource.isIn(DamageTypeTags.BYPASSES_INVULNERABILITY)) {
-				ServerWorld world = entity.world.getServer().getOverworld();
-				BlockPos worldSpawnPos = world.getSpawnPos();
+			entity.getWorld().playSound(null, entity.getBlockPos(), ModSoundEvents.ENTITY_GENERIC_RESURRECT, entity.getSoundCategory(), 1, 1);
+			if (entity.getY() <= entity.getWorld().getBottomY() && damageSource.isIn(DamageTypeTags.BYPASSES_INVULNERABILITY)) {
+				ServerWorld world = entity.getWorld().getServer().getOverworld();
+				BlockPos spawnPos = world.getSpawnPos();
 				if (entity instanceof ServerPlayerEntity player) {
-					world = entity.world.getServer().getWorld(player.getSpawnPointDimension());
+					world = entity.getWorld().getServer().getWorld(player.getSpawnPointDimension());
 					BlockPos playerSpawnPos = player.getSpawnPointPosition();
 					if (playerSpawnPos != null) {
-						worldSpawnPos = playerSpawnPos;
+						spawnPos = playerSpawnPos;
 					}
 				}
-				FabricDimensions.teleport(entity, world, new TeleportTarget(Vec3d.of(worldSpawnPos), Vec3d.ZERO, entity.headYaw, entity.getPitch()));
+				FabricDimensions.teleport(entity, world, new TeleportTarget(Vec3d.of(spawnPos), Vec3d.ZERO, entity.headYaw, entity.getPitch()));
 			}
 			entity.setHealth(entity.getMaxHealth());
 			ModEntityComponents.HOURAI.maybeGet(entity).ifPresent(houraiComponent -> houraiComponent.setWeaknessTimer(Math.min(houraiComponent.getWeaknessTimer() + 400, 1600)));
--- src/main/java/moriyashiine/houraielixir/mixin/LivingEntityMixin.java
+++ src/main/java/moriyashiine/houraielixir/mixin/LivingEntityMixin.java
@@ -25,7 +25,7 @@ public abstract class LivingEntityMixin extends Entity {
 
 	@Inject(method = "canHaveStatusEffect", at = @At("HEAD"), cancellable = true)
 	private void houraielixir$statusEffectImmunity(StatusEffectInstance effect, CallbackInfoReturnable<Boolean> cir) {
-		if (!world.isClient && HouraiElixir.isImmortal(LivingEntity.class.cast(this)) && ModEntityComponents.HOURAI.get(this).getWeaknessTimer() == 0 && effect.getEffectType().getCategory() != StatusEffectCategory.BENEFICIAL) {
+		if (!getWorld().isClient && HouraiElixir.isImmortal(LivingEntity.class.cast(this)) && ModEntityComponents.HOURAI.get(this).getWeaknessTimer() == 0 && effect.getEffectType().getCategory() != StatusEffectCategory.BENEFICIAL) {
 			cir.setReturnValue(false);
 		}
 	}
